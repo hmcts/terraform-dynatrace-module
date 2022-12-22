471a04cabad58dc96f9c2f8aec7c327304f74b97
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-12-22 12:20:05 +0000

    updated email body

--- 025-dynatrace-integration.tf
+++ 025-dynatrace-integration.tf
@@ -5,6 +5,6 @@ resource "dynatrace_email_notification" "email_integration" {
   subject    = var.email_subject
   to         = var.email_receivers
   cc         = var.email_cc_receivers
-  body       = "{ProblemDetailsHTML}"
+  body       = var.email_body
   depends_on = [dynatrace_alerting.alerts]
 }
